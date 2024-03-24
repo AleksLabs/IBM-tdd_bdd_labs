12b32bae34c2eaaef72f2d35bfe660281b414c3a
Aleksandr <alexk.vendors@gmail.com>
2024-03-24 12:19:40 +0100

    lab_07_done

--- labs/07_practicing_tdd/counter.py
+++ labs/07_practicing_tdd/counter.py
@@ -16,3 +16,38 @@ def create_counter(name):
 
     COUNTERS[name] = 0
     return { name: COUNTERS[name] }, status.HTTP_201_CREATED
+
+@app.route("/counters/<name>", methods=["PUT"])
+def read_counter(name):
+    """Updates a counter"""
+    app.logger.info(f"Request to update counter: {name}")
+    global COUNTERS
+
+    if name in COUNTERS:
+        COUNTERS[name] += 1
+        app.logger.info(f"Counter {name} is now {COUNTERS[name]}")
+        return { name: COUNTERS[name] }, status.HTTP_200_OK           
+    return {"message":f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+
+@app.route("/counters/<name>", methods=["GET"])
+def update_counter(name):
+    """Read a counter"""
+    app.logger.info(f"Request to read counter: {name}")
+    global COUNTERS
+
+    if name in COUNTERS:
+        app.logger.info(f"Counter {name} is {COUNTERS[name]}")
+        return { name: COUNTERS[name] }, status.HTTP_200_OK           
+    return {"message":f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+
+@app.route("/counters/<name>", methods=["DELETE"])
+def delete_counter(name):
+    """Read a counter"""
+    app.logger.info(f"Request to delete counter: {name}")
+    global COUNTERS
+
+    if name in COUNTERS:
+        del(COUNTERS[name])
+        app.logger.info(f"Counter {name} has been deleted")
+        return '', status.HTTP_204_NO_CONTENT          
+    return {"message":f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
