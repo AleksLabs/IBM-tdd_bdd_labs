7a0f177eb1b49da26e96840abd9fcdc4a9d5d338
Aleksandr <alexk.vendors@gmail.com>
2024-03-30 10:41:48 +0100

    almost all tdd\/bdd labs done

--- labs/10_loading_test_data/features/steps/load_steps.py
+++ labs/10_loading_test_data/features/steps/load_steps.py
@@ -11,3 +11,12 @@ from behave import given
 
 # Load data here
 
+@given("the following pets")
+def step_impl(context):
+    """Refreshing initial table"""
+    # List all of the pets and delete them one by one
+    response = requests.get(f"{context.base_url}/pets")
+    assert response.status_code == 200
+    for pet in response.json():
+        del_response = requests.delete(f"{context.base_url}/pets/{pet['id']}")
+        assert del_response.status_code == 204
